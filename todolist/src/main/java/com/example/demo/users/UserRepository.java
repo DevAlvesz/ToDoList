f7561f302295c4c877d78fdf80b6553148e6f4a7
Luiz Fellipe <luizfellipe.alves1@gmail.com>
2025-03-18 19:35:48 -0300

    Corrigindo ID de Long para UUID no UserModel

--- todolist/src/main/java/com/example/demo/users/UserRepository.java
+++ todolist/src/main/java/com/example/demo/users/UserRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.users;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import java.util.Optional;
-
-
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
     Optional<UserModel> findByUsername(String username);
 }
