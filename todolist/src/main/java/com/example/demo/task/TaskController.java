9bdaca9784ae4029a0d42800f74e9d359fdcbb0b
Luiz Fellipe <luizfellipe.alves1@gmail.com>
2025-03-24 23:06:59 -0300

    Adicionado a funcionalidade de listar as  tarefas.

--- todolist/src/main/java/com/example/demo/task/TaskController.java
+++ todolist/src/main/java/com/example/demo/task/TaskController.java
@@ -1,12 +1,14 @@
 package com.example.demo.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,12 +32,19 @@ public class TaskController {
         var currentDate = LocalDateTime.now();
         if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("A data de início/término deve ser maior que a data atual");
+            .body("A data de início/término deve ser maior que a data atual");
         }
 
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
 
 }
